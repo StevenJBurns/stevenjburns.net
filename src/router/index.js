2185c8cbdf8f948a95138286265b89744340a878
StevenJBurns <stevenjburns@msn.com>
2018-05-25 17:45:37 -0600

    Removed references in router component to previously deleted page components

--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,10 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import PageHome from '../components/pages/PageHome';
-import PageAbout from '../components/pages/PageAbout';
-//import HelloWorld from '@/components/HelloWorld';
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: PageHome
-    },
-    {
-      path: '/about',
-      name: 'About',
-      component: PageAbout
-    },
-    {
-      path: '/projects',
-      name: 'Projects',
-      component: PageProjects
-    },
-    {
-      path: '/sandbox',
-      name: 'Sandbox',
-      component: PageSandbox
-    }
+    
   ]
 })
